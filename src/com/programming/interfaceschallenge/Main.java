ecd8b546322cd466a176401ec2b5e700b4577408
muataz alsayed <muataz.cs@gmial.com>
2021-06-19 06:50:02 +0300

    updated ISaveable.java and created new class Player.java added functionality to the class Player.java - added saveObject method and load object method to the Main class and updated the main method in the same class

--- src/com/programming/interfaceschallenge/Main.java
+++ src/com/programming/interfaceschallenge/Main.java
@@ -7,6 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Player tim = new Player("Tim",10,15);
+        System.out.println(tim.toString());
+        saveObject(tim);
+
+        tim.setHitPoints(8);
+        System.out.println(tim);
+        tim.setWeapon("Stormbringer");
+        saveObject(tim);
+        loadObject(tim);
+        System.out.println(tim);
+
     }
 
     public static ArrayList<String> readValues(){
@@ -37,5 +48,25 @@ public class Main {
         }
 
         return values;
+    }
+
+    public static void saveObject(ISaveable objectToSave){
+
+        for (int i = 0; i < objectToSave.write().size(); i++){
+
+            System.out.println("Saving.. " + objectToSave.write().get(i) + " to storage device.");
+
+        }
+
+    }
+
+    public static void loadObject(ISaveable objectToLoad){
+
+        ArrayList<String> values = readValues();
+
+        objectToLoad.read(values);
+
+
+
     }
 }
