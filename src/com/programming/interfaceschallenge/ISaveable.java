d661d3bf283e2003f94355f68a612d6ad8971e4e
muataz alsayed <muataz.cs@gmial.com>
2021-06-19 06:12:30 +0300

    updated ISaveable.java and created new class Player.java added functionality to the class Player.java

--- src/com/programming/interfaceschallenge/ISaveable.java
+++ src/com/programming/interfaceschallenge/ISaveable.java
@@ -1,11 +1,12 @@
 package com.programming.interfaceschallenge;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface ISaveable{
 
-    ArrayList<String> write();
-    void read(ArrayList<String> savedValues);
+    List<String> write();
+    void read(List<String> savedValues);
 
             
 }
